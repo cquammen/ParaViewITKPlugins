03def8b10b9f65812131256f7dc4b4c867c72104
Cory Quammen <cquammen@cs.unc.edu>
2010-11-03 13:31:34 -0400

    Added base class for VTK-wrapped ITK image filters.
    This class takes care of setting up the glue between VTK and ITK
    so that plugins, which are defined as subclasses of this class,
    can be greatly simplified.

---
+++ Common/vtkITKImageFilter.cxx
@@ -0,0 +1,209 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkITKImageFilter.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkITKImageFilter.h"
+
+#include "vtkObjectFactory.h"
+#include "vtkImageExport.h"
+#include "vtkImageImport.h"
+#include "vtkImageData.h"
+#include "vtkInformationVector.h"
+#include "vtkInformation.h"
+
+vtkStandardNewMacro(vtkITKImageFilter);
+
+//----------------------------------------------------------------------------
+vtkITKImageFilter::vtkITKImageFilter()
+{
+  this->VTKCaster   = vtkImageCast::New();
+  this->VTKCaster->SetOutputScalarTypeToFloat();
+
+  this->VTKExporter = vtkImageExport::New();
+  this->ITKImporter = ITKImageImportType::New();
+  this->ITKExporter = ITKImageExportType::New();
+  this->VTKImporter = vtkImageImport::New();
+
+  this->InitializeITKImporters();
+  this->InitializeVTKImporters();
+}
+
+//----------------------------------------------------------------------------
+vtkITKImageFilter::~vtkITKImageFilter()
+{
+  if (this->VTKCaster)
+    {
+    this->VTKCaster->Delete();
+    }
+
+  if (this->VTKExporter)
+    {
+    this->VTKExporter->Delete();
+    }
+
+  if (this->VTKImporter)
+    {
+    this->VTKImporter->Delete();
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkITKImageFilter::InitializeITKImporters()
+{
+  // Connect the vtkImageCast that converts the input to the
+  // floating-point type we want to pass to ITK filters.
+  this->VTKExporter->SetInputConnection(this->VTKCaster->GetOutputPort());
+
+  // This call takes the place of the usual SetInput() method.
+  this->ITKImporter->SetCallbackUserData(this->VTKExporter);
+
+  // Set the rest of the callbacks
+  this->ITKImporter->SetBufferPointerCallback(this->VTKExporter->GetBufferPointerCallback());
+  this->ITKImporter->SetDataExtentCallback(this->VTKExporter->GetDataExtentCallback());
+  this->ITKImporter->SetOriginCallback(this->VTKExporter->GetOriginCallback());
+  this->ITKImporter->SetSpacingCallback(this->VTKExporter->GetSpacingCallback());
+  this->ITKImporter->SetNumberOfComponentsCallback(this->VTKExporter->GetNumberOfComponentsCallback());
+  this->ITKImporter->SetPipelineModifiedCallback(this->VTKExporter->GetPipelineModifiedCallback());
+  this->ITKImporter->SetPropagateUpdateExtentCallback(this->VTKExporter->GetPropagateUpdateExtentCallback());
+  this->ITKImporter->SetScalarTypeCallback(this->VTKExporter->GetScalarTypeCallback());
+  this->ITKImporter->SetUpdateDataCallback(this->VTKExporter->GetUpdateDataCallback());
+  this->ITKImporter->SetUpdateInformationCallback(this->VTKExporter->GetUpdateInformationCallback());
+  this->ITKImporter->SetWholeExtentCallback(this->VTKExporter->GetWholeExtentCallback());
+}
+
+//----------------------------------------------------------------------------
+void vtkITKImageFilter::InitializeVTKImporters()
+{
+  // This call takes the place of the usual SetInput() method.
+  this->VTKImporter->SetCallbackUserData(ITKExporter->GetCallbackUserData());
+
+  // Set the rest of the callbacks
+  this->VTKImporter->SetUpdateInformationCallback(ITKExporter->GetUpdateInformationCallback());
+  this->VTKImporter->SetPipelineModifiedCallback(ITKExporter->GetPipelineModifiedCallback());
+  this->VTKImporter->SetWholeExtentCallback(ITKExporter->GetWholeExtentCallback());
+  this->VTKImporter->SetSpacingCallback(ITKExporter->GetSpacingCallback());
+  this->VTKImporter->SetOriginCallback(ITKExporter->GetOriginCallback());
+  this->VTKImporter->SetScalarTypeCallback(ITKExporter->GetScalarTypeCallback());
+  this->VTKImporter->SetNumberOfComponentsCallback(ITKExporter->GetNumberOfComponentsCallback());
+  this->VTKImporter->SetPropagateUpdateExtentCallback(ITKExporter->GetPropagateUpdateExtentCallback());
+  this->VTKImporter->SetUpdateDataCallback(ITKExporter->GetUpdateDataCallback());
+  this->VTKImporter->SetDataExtentCallback(ITKExporter->GetDataExtentCallback());
+  this->VTKImporter->SetBufferPointerCallback(ITKExporter->GetBufferPointerCallback());
+}
+
+//----------------------------------------------------------------------------
+void vtkITKImageFilter::SetITKPipelineFirstFilter(ITKInternalFilterType* filter)
+{
+  if ( this->ITKPipelineFirstFilter != filter)
+    {
+    this->ITKPipelineFirstFilter = filter;
+    this->ITKPipelineFirstFilter->SetInput(this->ITKImporter->GetOutput());
+    this->Modified();
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkITKImageFilter::SetITKPipelineLastFilter(ITKInternalFilterType* filter)
+{
+  if ( this->ITKPipelineLastFilter != filter)
+    {
+    this->ITKPipelineLastFilter = filter;
+    this->ITKExporter->SetInput(filter->GetOutput());
+    this->Modified();
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkITKImageFilter::Init()
+{
+  this->InitializeITKImporters();
+  this->InitializeVTKImporters();
+}
+
+//----------------------------------------------------------------------------
+int vtkITKImageFilter::BeforeUpdateInternalFilters(vtkInformationVector** inputVector)
+{
+  // Check that the inputs are of type vtkImageData
+  vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
+  vtkImageData *input = vtkImageData::SafeDownCast
+    (inInfo->Get(vtkDataObject::DATA_OBJECT()));
+  if(!input)
+    {
+    vtkErrorMacro("An input is not of type vtkImageData");
+    return 0;
+    }
+
+  this->VTKCaster->SetInput(input);
+  this->VTKCaster->Update();
+
+  // Hook up to the beginning of the ITK pipeline
+  this->VTKExporter->SetInput(this->VTKCaster->GetOutput());
+
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+int vtkITKImageFilter::AfterUpdateInternalFilters(vtkInformationVector* outputVector)
+{
+  // Now connect the ITK pipeline output to the VTK output
+  this->ITKExporter->Update();
+
+  vtkInformation *outInfo = outputVector->GetInformationObject(0);
+  vtkImageData *output = vtkImageData::SafeDownCast
+    (outInfo->Get(vtkDataObject::DATA_OBJECT()));
+  if (!output)
+    {
+    vtkErrorMacro("Output is not of type vtkImageData");
+    return 0;
+    }
+
+  this->VTKImporter->Update();
+  this->VTKImporter->Update();
+
+  output->ShallowCopy(this->VTKImporter->GetOutput());
+
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+int vtkITKImageFilter::RequestData(vtkInformation *request,
+                                   vtkInformationVector **inputVector,
+                                   vtkInformationVector *outputVector)
+{
+  try
+    {
+    if (!this->BeforeUpdateInternalFilters(inputVector))
+      {
+      return 0;
+      }
+
+    this->UpdateInternalFilters();
+
+    if (!this->AfterUpdateInternalFilters(outputVector))
+      {
+      return 0;
+      }
+    }
+  catch(itk::ExceptionObject& error)
+    {
+    return 0;
+    }
+
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+void vtkITKImageFilter::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
---
+++ Common/vtkITKImageFilter.h
@@ -0,0 +1,95 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkITKImageFilter.h
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+// .NAME vtkITKImageFilter - base class for VTK classes that wrap ITK filters
+//
+// .SECTION Description
+
+#ifndef __vtkITKImageFilter_h
+#define __vtkITKImageFilter_h
+
+#include <vtkImageCast.h>
+
+#include <itkImageToImageFilter.h>
+#include <itkVTKImageImport.h>
+#include <itkVTKImageExport.h>
+
+class vtkImageExport;
+class vtkImageImport;
+
+class VTK_EXPORT vtkITKImageFilter : public vtkImageAlgorithm
+{
+public:
+
+  //BTX
+  typedef float                                                 PixelType;
+  typedef itk::Image< PixelType, 3 >                            ITKImageType;
+  typedef itk::ImageToImageFilter< ITKImageType, ITKImageType > ITKInternalFilterType;
+  typedef itk::VTKImageImport< ITKImageType >                   ITKImageImportType;
+  typedef itk::VTKImageExport< ITKImageType >                   ITKImageExportType;
+  //ETX
+
+  static vtkITKImageFilter* New();
+  vtkTypeMacro(vtkITKImageFilter, vtkImageAlgorithm);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+protected:
+  vtkITKImageFilter();
+  virtual ~vtkITKImageFilter();
+
+
+  //BTX
+  ITKInternalFilterType*      ITKPipelineFirstFilter;
+  ITKInternalFilterType*      ITKPipelineLastFilter;
+  vtkImageCast*               VTKCaster;
+  vtkImageExport*             VTKExporter;
+  ITKImageImportType::Pointer ITKImporter;
+  ITKImageExportType::Pointer ITKExporter;
+  vtkImageImport*             VTKImporter;
+  //ETX
+
+protected:
+  void InitializeITKImporters();
+  void InitializeVTKImporters();
+
+  // DESCRIPTION:
+  // Set ITK filter whose output should be converted back to VTK.
+  void SetITKPipelineFirstFilter(ITKInternalFilterType* filter);
+  void SetITKPipelineLastFilter(ITKInternalFilterType* filter);
+
+  // DESCRIPTION:
+  // Subclasses must call this method *after* specifying the number of
+  // input and output ports of the VTK filter.
+  void Init();
+
+  // DESCRIPTION:
+  // This is called prior to the UpdateInternalFilters method.
+  virtual int BeforeUpdateInternalFilters(vtkInformationVector** inputVector);
+
+  // DESCRIPTION:
+  // This is called after the UpdateInternalFilters method.
+  virtual int AfterUpdateInternalFilters(vtkInformationVector* outputVector);
+
+  // DESCRIPTION:
+  // This is where all internal filter parameters should be updated.
+  virtual int UpdateInternalFilters() {return 1;};
+
+  int RequestData(vtkInformation *request, vtkInformationVector **inputVector, vtkInformationVector *outputVector);
+
+private:
+  vtkITKImageFilter(const vtkITKImageFilter&);  // Not implemented.
+  void operator=(const vtkITKImageFilter&);  // Not implemented.
+};
+
+#endif
