93c16b0a7463ab4cebc716567edcf4150b1b41ee
Cory Quammen <cquammen@cs.unc.edu>
2010-11-02 22:54:45 -0400

    Added Wiener deconvolution image filter plugin.

---
+++ DeconvolutionFilters/vtkWienerDeconvolutionImageFilter.cxx
@@ -0,0 +1,250 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkWienerDeconvolutionImageFilter.cxx
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#include "vtkWienerDeconvolutionImageFilter.h"
+
+#include "vtkObjectFactory.h"
+#include "vtkImageExport.h"
+#include "vtkImageImport.h"
+#include "vtkImageData.h"
+#include "vtkInformationVector.h"
+#include "vtkInformation.h"
+#include "vtkImageShiftScale.h"
+#include "vtkStreamingDemandDrivenPipeline.h"
+
+#include "itkWienerDeconvolutionImageFilter.h"
+
+// Warning: this is a hack!
+#include "itkFFTWLock.cxx"
+
+
+vtkStandardNewMacro(vtkWienerDeconvolutionImageFilter);
+
+//----------------------------------------------------------------------------
+vtkWienerDeconvolutionImageFilter::vtkWienerDeconvolutionImageFilter()
+{
+  this->SetNumberOfInputPorts(2);
+  this->SetNumberOfOutputPorts(1);
+
+  this->VTKExporter            = vtkImageExport::New();
+  this->VTKKernelExporter      = vtkImageExport::New();
+  this->ITKImporter            = ITKImageImportType::New();
+  this->ITKKernelImporter      = ITKImageImportType::New();
+  this->ITKDeconvolutionFilter = ITKDeconvolutionFilterType::New();
+  this->ITKExporter            = ITKImageExportType::New();
+  this->VTKImporter            = vtkImageImport::New();
+
+  this->InitializeITKImporters();
+  this->InitializeITKExporter();
+}
+
+//----------------------------------------------------------------------------
+vtkWienerDeconvolutionImageFilter::~vtkWienerDeconvolutionImageFilter()
+{
+  if (this->VTKExporter)
+    {
+    this->VTKExporter->Delete();
+    }
+
+  if (this->VTKKernelExporter)
+    {
+    this->VTKKernelExporter->Delete();
+    }
+
+  if (this->VTKImporter)
+    {
+    this->VTKImporter->Delete();
+    }
+}
+
+//----------------------------------------------------------------------------
+void vtkWienerDeconvolutionImageFilter::SetKernelImage(vtkAlgorithmOutput* image)
+{
+  this->SetInputConnection(1, image);
+}
+
+//----------------------------------------------------------------------------
+void vtkWienerDeconvolutionImageFilter::InitializeITKImporters()
+{
+  // This call takes the place of the usual SetInput() method.
+  this->ITKImporter->SetCallbackUserData(this->VTKExporter);
+
+  // Set the rest of the callbacks
+  this->ITKImporter->SetBufferPointerCallback(this->VTKExporter->GetBufferPointerCallback());
+  this->ITKImporter->SetDataExtentCallback(this->VTKExporter->GetDataExtentCallback());
+  this->ITKImporter->SetOriginCallback(this->VTKExporter->GetOriginCallback());
+  this->ITKImporter->SetSpacingCallback(this->VTKExporter->GetSpacingCallback());
+  this->ITKImporter->SetNumberOfComponentsCallback(this->VTKExporter->GetNumberOfComponentsCallback());
+  this->ITKImporter->SetPipelineModifiedCallback(this->VTKExporter->GetPipelineModifiedCallback());
+  this->ITKImporter->SetPropagateUpdateExtentCallback(this->VTKExporter->GetPropagateUpdateExtentCallback());
+  this->ITKImporter->SetScalarTypeCallback(this->VTKExporter->GetScalarTypeCallback());
+  this->ITKImporter->SetUpdateDataCallback(this->VTKExporter->GetUpdateDataCallback());
+  this->ITKImporter->SetUpdateInformationCallback(this->VTKExporter->GetUpdateInformationCallback());
+  this->ITKImporter->SetWholeExtentCallback(this->VTKExporter->GetWholeExtentCallback());
+
+  // This call takes the place of the usual SetInput() method.
+  this->ITKKernelImporter->SetCallbackUserData(this->VTKKernelExporter);
+
+  // Set the rest of the callbacks
+  this->ITKKernelImporter->SetBufferPointerCallback(this->VTKKernelExporter->GetBufferPointerCallback());
+  this->ITKKernelImporter->SetDataExtentCallback(this->VTKKernelExporter->GetDataExtentCallback());
+  this->ITKKernelImporter->SetOriginCallback(this->VTKKernelExporter->GetOriginCallback());
+  this->ITKKernelImporter->SetSpacingCallback(this->VTKKernelExporter->GetSpacingCallback());
+  this->ITKKernelImporter->SetNumberOfComponentsCallback(this->VTKKernelExporter->GetNumberOfComponentsCallback());
+  this->ITKKernelImporter->SetPipelineModifiedCallback(this->VTKKernelExporter->GetPipelineModifiedCallback());
+  this->ITKKernelImporter->SetPropagateUpdateExtentCallback(this->VTKKernelExporter->GetPropagateUpdateExtentCallback());
+  this->ITKKernelImporter->SetScalarTypeCallback(this->VTKKernelExporter->GetScalarTypeCallback());
+  this->ITKKernelImporter->SetUpdateDataCallback(this->VTKKernelExporter->GetUpdateDataCallback());
+  this->ITKKernelImporter->SetUpdateInformationCallback(this->VTKKernelExporter->GetUpdateInformationCallback());
+  this->ITKKernelImporter->SetWholeExtentCallback(this->VTKKernelExporter->GetWholeExtentCallback());
+}
+
+//----------------------------------------------------------------------------
+void vtkWienerDeconvolutionImageFilter::InitializeITKExporter()
+{
+  // This call takes the place of the usual SetInput() method.
+  this->VTKImporter->SetCallbackUserData(ITKExporter->GetCallbackUserData());
+
+  // Set the rest of the callbacks
+  this->VTKImporter->SetUpdateInformationCallback(ITKExporter->GetUpdateInformationCallback());
+  this->VTKImporter->SetPipelineModifiedCallback(ITKExporter->GetPipelineModifiedCallback());
+  this->VTKImporter->SetWholeExtentCallback(ITKExporter->GetWholeExtentCallback());
+  this->VTKImporter->SetSpacingCallback(ITKExporter->GetSpacingCallback());
+  this->VTKImporter->SetOriginCallback(ITKExporter->GetOriginCallback());
+  this->VTKImporter->SetScalarTypeCallback(ITKExporter->GetScalarTypeCallback());
+  this->VTKImporter->SetNumberOfComponentsCallback(ITKExporter->GetNumberOfComponentsCallback());
+  this->VTKImporter->SetPropagateUpdateExtentCallback(ITKExporter->GetPropagateUpdateExtentCallback());
+  this->VTKImporter->SetUpdateDataCallback(ITKExporter->GetUpdateDataCallback());
+  this->VTKImporter->SetDataExtentCallback(ITKExporter->GetDataExtentCallback());
+  this->VTKImporter->SetBufferPointerCallback(ITKExporter->GetBufferPointerCallback());
+}
+
+//----------------------------------------------------------------------------
+int vtkWienerDeconvolutionImageFilter::RequestData(vtkInformation *request,
+					    vtkInformationVector **inputVector,
+					    vtkInformationVector *outputVector)
+{
+  vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
+  vtkImageData *input = vtkImageData::SafeDownCast
+    (inInfo->Get(vtkDataObject::DATA_OBJECT()));
+  if(!input)
+    {
+    vtkErrorMacro("Input is not of type vtkImageData");
+    return 0;
+    }
+  vtkInformation *kernelInfo = inputVector[1]->GetInformationObject(0);
+  vtkImageData *kernelInput = vtkImageData::SafeDownCast
+    (kernelInfo->Get(vtkDataObject::DATA_OBJECT()));
+  if (!kernelInfo)
+    {
+    vtkErrorMacro("Kernel is not of type vtkImageData");
+    return 0;
+    }
+
+  vtkImageShiftScale* castInput = vtkImageShiftScale::New();
+  castInput->SetOutputScalarTypeToFloat();
+  castInput->SetInput(input);
+  castInput->Update();
+
+  vtkImageShiftScale* castKernel = vtkImageShiftScale::New();
+  castKernel->SetOutputScalarTypeToFloat();
+  castKernel->SetInput(kernelInput);
+  castKernel->Update();
+
+  // Hook up to the beginning of the ITK pipeline
+  this->VTKExporter->SetInput(castInput->GetOutput());
+  this->VTKKernelExporter->SetInput(castKernel->GetOutput());
+
+  try
+    {
+    // Now connect the ITK pipeline output to the VTK output
+    this->ITKDeconvolutionFilter->SetInput(this->ITKImporter->GetOutput());
+    this->ITKDeconvolutionFilter->SetKernelImage(this->ITKKernelImporter->GetOutput());
+    this->ITKDeconvolutionFilter->SetPadMethod(this->PaddingMethod);
+    this->ITKDeconvolutionFilter->Update();
+
+    this->ITKExporter->SetInput(this->ITKDeconvolutionFilter->GetOutput());
+
+    this->ITKExporter->Update();
+
+    vtkInformation *outInfo = outputVector->GetInformationObject(0);
+    vtkImageData *output = vtkImageData::SafeDownCast
+      (outInfo->Get(vtkDataObject::DATA_OBJECT()));
+    if (!output)
+      {
+      vtkErrorMacro("Output is not of type vtkImageData");
+      return 0;
+      }
+
+    // For some reason, updates don't seem to go through unless you call
+    // the following twice.
+    this->VTKImporter->Update();
+    this->VTKImporter->Update();
+
+    output->DeepCopy(this->VTKImporter->GetOutput());
+    }
+  catch (itk::ExceptionObject& error)
+    {
+
+    }
+
+  castInput->Delete();
+  castKernel->Delete();
+
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+int vtkWienerDeconvolutionImageFilter::RequestUpdateExtent (
+  vtkInformation * vtkNotUsed(request),
+  vtkInformationVector **inputVector,
+  vtkInformationVector *outputVector)
+{
+  // get the info objects
+  vtkInformation* inInfo1 = inputVector[0]->GetInformationObject(0);
+  vtkInformation* inInfo2 = inputVector[1]->GetInformationObject(0);
+
+  // Update the whole extent of both the input image and kernel image
+  int inWExt1[6];
+  inInfo1->Get(vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT(), inWExt1);
+  inInfo1->Set(vtkStreamingDemandDrivenPipeline::UPDATE_EXTENT(), inWExt1, 6);
+
+  // get the whole image for input 2
+  int inWExt2[6];
+  inInfo2->Get(vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT(), inWExt2);
+  inInfo2->Set(vtkStreamingDemandDrivenPipeline::UPDATE_EXTENT(), inWExt2, 6);
+
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+int vtkWienerDeconvolutionImageFilter::FillInputPortInformation(int port, vtkInformation *info)
+{
+  info->Set(vtkAlgorithm::INPUT_REQUIRED_DATA_TYPE(), "vtkImageData");
+  info->Set(vtkAlgorithm::INPUT_IS_REPEATABLE(), 0);
+  info->Set(vtkAlgorithm::INPUT_IS_OPTIONAL(), 0);
+
+  if (port == 0 || port == 1)
+    {
+    return 1;
+    }
+
+  return 0;
+}
+
+//----------------------------------------------------------------------------
+void vtkWienerDeconvolutionImageFilter::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
