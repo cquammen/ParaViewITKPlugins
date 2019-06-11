0db309ee346796450855c83f897bc5ebe2e574d9
Sankhesh Jhaveri <sankhesh.jhaveri@kitware.com>
2019-06-11 18:33:13 -0400

    Add a paraview plugin for itkCoocurrenceTextureFeaturesImageFilter

---
+++ CoocurrenceTextureFeaturesImageFilter/vtk/vtkCoocurrenceTextureFeaturesImageFilter.cxx
@@ -0,0 +1,53 @@
+
+#include "vtkCoocurrenceTextureFeaturesImageFilter.h"
+
+#include "vtkObjectFactory.h"
+#include "vtkImageExport.h"
+#include "vtkImageImport.h"
+#include "vtkImageData.h"
+#include "vtkInformationVector.h"
+#include "vtkInformation.h"
+
+vtkStandardNewMacro(vtkCoocurrenceTextureFeaturesImageFilter);
+
+//----------------------------------------------------------------------------
+vtkCoocurrenceTextureFeaturesImageFilter::vtkCoocurrenceTextureFeaturesImageFilter()
+{
+  // Set up number of input and output ports
+
+  // Initialize the ITK-VTK glue.
+  this->Init();
+
+  // Set up the internal filter pipeline.
+  this->TextureFeaturesFilter = TextureFeaturesFilterType::New();
+  // this->RelabelFilter = RelabelType::New();
+  // this->caster = CastingFilterType::New();
+
+  // Set the first and last filters in the internal ITK pipeline.
+  this->SetITKPipelineFirstFilter<TextureFeaturesFilterType>(this->TextureFeaturesFilter);
+  this->SetITKPipelineLastFilter<TextureFeaturesFilterType>(this->TextureFeaturesFilter);
+}
+
+//----------------------------------------------------------------------------
+vtkCoocurrenceTextureFeaturesImageFilter::~vtkCoocurrenceTextureFeaturesImageFilter()
+{
+}
+
+//----------------------------------------------------------------------------
+int vtkCoocurrenceTextureFeaturesImageFilter::UpdateInternalFilters()
+{
+  this->TextureFeaturesFilter->SetNumberOfBinsPerAxis(this->numberOfBinsPerAxis); 
+  this->TextureFeaturesFilter->SetHistogramMinimum(this->histMinimum); 
+  this->TextureFeaturesFilter->SetHistogramMaximum(this->histMaximum); 
+  this->Neighborhood.SetRadius(neighborhoodRadius);
+  this->TextureFeaturesFilter->SetNeighborhoodRadius(Neighborhood.GetRadius());
+  this->TextureFeaturesFilter->Update();
+
+  return 1;
+}
+
+//----------------------------------------------------------------------------
+void vtkCoocurrenceTextureFeaturesImageFilter::PrintSelf(ostream& os, vtkIndent indent)
+{
+  this->Superclass::PrintSelf(os,indent);
+}
---
+++ CoocurrenceTextureFeaturesImageFilter/vtk/vtkCoocurrenceTextureFeaturesImageFilter.h
@@ -0,0 +1,98 @@
+
+#ifndef __vtkCoocurrenceTextureFeaturesImageFilter_h
+#define __vtkCoocurrenceTextureFeaturesImageFilter_h
+
+#include <vtkITKImageFilter.h>
+
+#include <itkCoocurrenceTextureFeaturesImageFilter.h>
+#include <itkRelabelComponentImageFilter.h>
+#include <itkBinaryThresholdImageFilter.h>
+#include <itkCastImageFilter.h>
+
+class VTK_EXPORT vtkCoocurrenceTextureFeaturesImageFilter : public vtkITKImageFilter
+{
+public:
+
+  static vtkCoocurrenceTextureFeaturesImageFilter* New();
+  vtkTypeMacro(vtkCoocurrenceTextureFeaturesImageFilter, vtkITKImageFilter);
+  void PrintSelf(ostream& os, vtkIndent indent);
+
+  //BTX
+  // typedef Superclass::PixelType                PixelType;
+  // typedef Superclass::ITKVectorPixelType       VectorPixelType;
+  typedef Superclass::ITKIntImageType             InputImageType;
+  typedef Superclass::ITKFloatVectorImageType       OutputImageType;
+  // typedef Superclass::ITKInternalFilterType ITKInternalFilterType;
+  // typedef Superclass::ITKImageImportType    ITKImageImportType;
+  // typedef Superclass::ITKImageExportType    ITKImageExportType;
+
+  // typedef itk::Image< float , 3 >  InputImageType;
+  // typedef itk::Image< itk::Vector< float, 10 > , 3 >  OutputImageType;
+
+  typedef itk::Neighborhood<typename InputImageType::PixelType, InputImageType::ImageDimension >
+    NeighborhoodType;
+    // using NeighborhoodType = itk::Neighborhood<typename InputImageType::PixelType,
+    //   InputImageType::ImageDimension>;
+
+  // typedef itk::CastImageFilter< InternalImageType, ITKImageType > CastingFilterType;
+  // typedef itk::BinaryThresholdImageFilter< ITKImageType, ITKImageType > ThresholdFilterType;
+  typedef itk::Statistics::CoocurrenceTextureFeaturesImageFilter< InputImageType, OutputImageType > 
+    TextureFeaturesFilterType;
+  // typedef itk::RelabelComponentImageFilter< InternalImageType, ITKImageType > RelabelType;
+  //ETX
+
+  // void SetLowerThreshold(double val) { LowerThreshold = val; this->Modified(); }
+  // double GetLowerThreshold() { return LowerThreshold; }
+  // void SetUpperThreshold(double val) { UpperThreshold = val; this->Modified(); }
+  // double GetUpperThreshold() { return UpperThreshold; }
+  // void SetFullyConnected(int ival) { FullyConnected = ival; this->Modified();}
+  // int GetFullyConnected() { return FullyConnected; }
+  // void SetMinimumObjectSize(int ival) { MinimumObjectSize = ival; this->Modified();}
+  // int GetMinimumObjectSize() { return MinimumObjectSize; }
+  void SetNumberOfBinsPerAxis(int val) { numberOfBinsPerAxis = val; this->Modified(); }
+  int GetNumberOfBinsPerAxis() { return numberOfBinsPerAxis; }
+  void SetHistogramMinimum(double val) { histMinimum = val; this->Modified(); }
+  double GetHistogramMinimum() { return histMinimum; }
+  void SetHistogramMaximum(double val) { histMaximum = val; this->Modified(); }
+  double GetHistogramMaximum() { return histMaximum; }
+  void SetNeighborhoodRadius(int val) { neighborhoodRadius = val; this->Modified(); }
+  int GetNeighborhoodRadius() { return neighborhoodRadius; }
+  
+  vtkSetMacro(numberOfBinsPerAxis, int);
+  vtkGetMacro(numberOfBinsPerAxis, int);
+  vtkSetMacro(histMinimum, double);
+  vtkGetMacro(histMinimum, double);
+  vtkSetMacro(histMaximum, double);
+  vtkGetMacro(histMaximum, double);
+  vtkSetMacro(neighborhoodRadius, int);
+  vtkGetMacro(neighborhoodRadius, int);
+  
+  void SetInputConnection(int port, vtkAlgorithmOutput* input)
+    {vtkImageAlgorithm::SetInputConnection(port, input);}
+  void SetInputConnection(vtkAlgorithmOutput* input)
+    {vtkImageAlgorithm::SetInputConnection(input);}
+
+
+protected:
+  vtkCoocurrenceTextureFeaturesImageFilter();
+  ~vtkCoocurrenceTextureFeaturesImageFilter();
+
+  int numberOfBinsPerAxis;
+  double histMinimum;
+  double histMaximum;
+  int neighborhoodRadius;
+
+  //BTX
+  NeighborhoodType Neighborhood;  // MJS FIXME Why can't this be a pointer?
+  TextureFeaturesFilterType::Pointer TextureFeaturesFilter;
+  //ETX
+
+protected:
+  int UpdateInternalFilters();
+
+private:
+  vtkCoocurrenceTextureFeaturesImageFilter(const vtkCoocurrenceTextureFeaturesImageFilter&);  // Not implemented.
+  void operator=(const vtkCoocurrenceTextureFeaturesImageFilter&);  // Not implemented.
+};
+
+#endif
